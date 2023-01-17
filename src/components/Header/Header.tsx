bf5bc8669ba1bc106638b0c768fdef3547bc3f91
Dimkles <dimkless.sharabaiko@gmail.com>
2023-01-17 21:44:25 +0300

    create menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Container from '../Container/Container';
+import Menu from '../Menu/Menu';
 import classes from './Header.module.scss'
 const Header = () => {
     return (
         <div className={classes.header}>
             <Container>
-                Header
+                <Menu />
             </Container>
         </div>
     );
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import classes from './Menu.module.scss'
 const Menu = () => {
+    const location = useLocation();
+    useEffect(() => {
+        setActiveLink(location.pathname)
+        console.log(location.pathname)
+    }, [location])
+    const [activeLink, setActiveLink] = useState('/')
+    const routes = [
+        { name: 'Главная', to: '/' },
+        { name: 'Товара', to: '/catalog' }
+    ]
     return (
         <nav className={classes.menu}>
-            МЕНЮ
+            <ul className={classes.menu__list}>
+                {routes.map((route) =>
+                    <li className={classes.menu__item} key={route.name}>
+                        <Link
+                            className={activeLink === route.to
+                                ? [classes.menu__link, classes.active].join(' ')
+                                : classes.menu__link}
+                            to={route.to}
+                        >
+                            {route.name}</Link>
+                    </li>
+                )}
+            </ul>
         </nav>
     );
 };
