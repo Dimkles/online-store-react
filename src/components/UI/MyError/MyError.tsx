9cae64978438b7a3e9c371b1f47d4af1586bc4cc
Dimkles <dimkless.sharabaiko@gmail.com>
2023-01-28 15:06:19 +0300

    изменил компонент ошибки

--- src/components/UI/MyError/MyError.tsx
+++ src/components/UI/MyError/MyError.tsx
@@ -1,10 +1,8 @@
 import React, { FC, ReactNode } from 'react';
 import classes from './MyError.module.scss'
-
 interface MyErrorProps {
     children: ReactNode
 }
-
 const MyError: FC<MyErrorProps> = ({ children }) => {
     return (
         <span className={classes.error}>
