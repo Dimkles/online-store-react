0b4669cc561168e073ce45d400d5c78e6b9adac1
Dimkles <dimkless.sharabaiko@gmail.com>
2023-04-18 13:52:41 +0300

    s

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Container from '../../components/UI/Container/Container';
+import MyImage from '../../components/UI/MyImage/MyImage';
+import { BACKEND_URL } from '../../consts/store';
 import { useFetchProductByIdQuery } from '../../services/RTK/ProductsService';
 
 const ProductPage = () => {
     const { id } = useParams()
 
-    const { data: product, isLoading } = useFetchProductByIdQuery({ id: id })
+    const { data: product, error } = useFetchProductByIdQuery({ id: id })
+    if (error) {
+        console.log(error)
+    }
 
 
     return (
         <Container>
-            {product?.name}
+            <>
+                {product?.name}
+                {product?.price}
+                {product?.quantity}
+                <MyImage webp={`${BACKEND_URL}/${product?.imagewebp}`} jpg={`${BACKEND_URL}/${product?.imagejpg}`} height={300} />
+            </>
         </Container>
     )
 };
